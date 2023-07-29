f57a0688d213fef2e04ffbf0aeb7ae132d6942a8
mimshahi <meisamshahi04@yahoo.com>
2023-07-29 17:25:57 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xde4133727f682b3b4c124edda68e68f33f7f013c";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
